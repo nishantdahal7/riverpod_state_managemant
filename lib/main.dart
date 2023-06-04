6ef52b6c45fecbf0766c3f5ec3fd36af5cda5f49
nishantdahal7 <dahalnishant22@gmail.com>
2023-06-04 11:04:55 +0545

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'view/add_view.dart';
 import 'view/counter_view.dart';
@@ -6,14 +7,17 @@ import 'view/dashboard_view.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      title: 'Riverpod state management',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const DashboardView(),
-        '/counter': (context) => const CounterView(),
-        '/add': (context) => const AddView(),
-      },
+    ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Riverpod state management',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const DashboardView(),
+          '/counter': (context) => const CounterView(),
+          '/add': (context) => const AddView(),
+        },
+      ),
     ),
   );
 }
--- lib/view/counter_view.dart
+++ lib/view/counter_view.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CounterView extends StatelessWidget {
+final numberProvider = Provider<int>((ref) => 100);
+
+final nameProvider = Provider<String>((ref) => "Hello World!!");
+
+//Read a provider
+//ref.read(numberProvider)
+//ref.read(nameProvider)
+
+//ProviderRef = Provider -> provider
+//WidgetRef = Widget -> provider
+class CounterView extends ConsumerWidget {
   const CounterView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter'),
       ),
-      body: const Center(
-        child: Text(
-          '1',
-          style: TextStyle(
-            fontSize: 24,
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              ref.read(numberProvider).toString(),
+              style: const TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            Text(
+              ref.read(nameProvider),
+              style: const TextStyle(fontSize: 24),
+            )
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
